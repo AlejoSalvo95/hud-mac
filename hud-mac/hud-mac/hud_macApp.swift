9e5df36d262fd2e65616699bba19820f6cbcb722
Ale <alejo.salvo@eneco.com>
2024-03-25 14:29:24 +0100

    XCode config

---
+++ hud-mac/hud-mac/hud_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  hud_macApp.swift
+//  hud-mac
+//
+//  Created by Alejo Salvo on 25/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct hud_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
